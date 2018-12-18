fabae4e3134b46166251cdb713a373aa39a9dea2
Rohan Richards <rohan.m.richards@gmail.com>
2018-12-18 16:27:21 +1000

    CORS override

--- config/lib/express.js
+++ config/lib/express.js
@@ -22,6 +22,19 @@ var config = require('../config'),
 	csrf = require('csurf'),
 	jwt = require('express-jwt');
 
+var allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+	// intercept OPTIONS method
+	if('OPTIONS' == req.method) {
+		res.send(200);
+	} else {
+		next();
+	}
+};
+
 /**
  * Initialize local variables
  */
@@ -68,6 +81,8 @@ module.exports.initMiddleware = function (app) {
 
 	// Enable jsonp
 	app.enable('jsonp callback');
+
+	app.use(allowCrossDomain);
 
 	// Should be placed before express.static
 	app.use(compress({
