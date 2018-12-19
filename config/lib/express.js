5b9f53b559a8e714255abc26247b40e29003c3e0
Rohan Richards <rohan.m.richards@gmail.com>
2018-12-19 12:48:43 +1000

    new allow header

--- config/lib/express.js
+++ config/lib/express.js
@@ -27,7 +27,7 @@ var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Origin', req.headers.origin);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-CSRF-Token');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, x-xsrf-token');
 
 	// intercept OPTIONS method
 	if('OPTIONS' == req.method) {
