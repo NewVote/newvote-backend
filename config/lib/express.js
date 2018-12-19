ba23aecab180837f7653345f63eb19a09dcf3cd2
Rohan Richards <rohan.m.richards@gmail.com>
2018-12-19 12:12:52 +1000

    resetting settings

--- config/lib/express.js
+++ config/lib/express.js
@@ -131,8 +131,8 @@ module.exports.initMiddleware = function (app) {
 	}));
 
 	app.use(function (req, res, next) {
-		console.log('setting cookie: ', req.csrfToken());
-		res.cookie('XSRF-TOKEN', req.csrfToken(), { domain: '.newvote.org', sameSite: 'Lax', httpOnly: false });
+		// console.log('setting cookie: ', req.csrfToken());
+		res.cookie('X-XSRF-TOKEN', req.csrfToken());
 		next();
 	});
 
