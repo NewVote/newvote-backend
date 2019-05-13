9a459a9d1bdead0313e766bc7835d3d0f525dfd6
Jamie <jamieplayer@gmail.com>
2019-05-13 22:27:11 +0100

    updated put request to search DB for user before updating and saving

--- modules/users/controllers/users.profile.server.controller.js
+++ modules/users/controllers/users.profile.server.controller.js
@@ -18,33 +18,42 @@ var _ = require('lodash'),
 exports.update = function (req, res) {
 	// Init Variables
 	var user = req.user;
-
 	// For security measurement we remove the roles from the req.body object
 	delete req.body.roles;
 
 	if(user) {
-		// Merge existing user
-		user = _.extend(user, req.body);
-		user.updated = Date.now();
-		user.displayName = user.firstName + ' ' + user.lastName;
+		// Find existing user on Database
+		User.findById(user._id)
+			.then(account => {
 
-		user.save(function (err) {
-			if(err) {
-				return res.status(400)
-					.send({
-						message: errorHandler.getErrorMessage(err)
-					});
-			} else {
-				req.login(user, function (err) {
-					if(err) {
-						res.status(400)
-							.send(err);
-					} else {
-						res.json(user);
+				// Merge existing user
+				account = _.extend(account, req.body);
+				account.updated = Date.now();
+				account.displayName = account.firstName + ' ' + account.lastName;
+				
+				return account;
+			})
+			.then(account => {
+				// Handle saving account
+				return account.save(function (err) {
+					if (err) {
+						return res.status(400)
+							.send({
+								message: errorHandler.getErrorMessage(err)
+							});
 					}
-				});
-			}
-		});
+					else {
+						req.login(account, function (err) {
+							if(err) {
+								res.status(400)
+									.send(err);
+							} else {
+								res.json(account);
+							}
+						});
+					}
+				})
+			})
 	} else {
 		res.status(400)
 			.send({
