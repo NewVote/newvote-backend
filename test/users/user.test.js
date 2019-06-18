f0d5ac9b6b7c430f7b927c0f4ce220906e5f33f8
Jamie <jamieplayer@gmail.com>
2019-06-19 00:00:53 +0100

    user model test

---
+++ test/users/user.test.js
@@ -0,0 +1,48 @@
+process.env.NODE_ENV = 'test';
+
+let mongoose = require('mongoose');
+let User = mongoose.model('User');
+
+// Dependencies
+let chai = require('chai');
+let should = chai.should();
+
+
+describe('User', () => {
+    beforeEach((done) => {
+        User.remove({}, (err) => {
+            done();
+        })
+    })
+});
+
+describe('Save', function () {
+    before(function (done) {
+
+        let newUser = {
+            firstName: 'testuser-1',
+            lastName: '',
+            displayName: 'testuser',
+            email: 'testuser1@testuser.com',
+            username: 'testuser1@testuser.com',
+            verified: 'true',
+            password: 'test123'
+        }
+        let user = new User(newUser);
+        user.save((err) => done());
+    });
+
+    after(function (done) {
+        User.remove({}, (err) => {
+            done();
+        });
+    })
+    
+    it('Should have one User', () => {
+        return User.find({})
+            .then((users) => {
+                users.should.be.a('number');
+                users.length.should.equal(1);            
+            })
+    })
+})
