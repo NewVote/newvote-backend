ca046ac204159ef946ed81fa15c6e93048c87f98
Jamie <jamieplayer@gmail.com>
2019-06-26 01:41:09 +0100

    seeding database on test run + EJSON package to handle mongo extended json files

---
+++ seed/media/media.seed.js
@@ -0,0 +1,7 @@
+let EJSON = require('mongodb-extjson');
+let doc = require('./media.seed.json');
+
+doc = EJSON.stringify(doc);
+doc = EJSON.parse(doc);
+
+module.exports = doc;
--- test/test.js
+++ test/test.js
@@ -10,6 +10,32 @@ var path = require('path');
 var config = require(path.resolve('./config/config'));
 var server = require('../config/lib/express').init();
 
+// Seed data
+const { Seeder } = require('mongo-seeding');
+
+const seedConfig = {
+    database: 'mongodb://localhost:27017/newvote-test',
+    dropDatabase: true
+}
+
+const seeder = new Seeder(seedConfig);
+const collectionReadingOptions = {
+    extensions: ['js']
+};
+
+const collections = seeder.readCollectionsFromPath(
+    path.resolve('./seed'),
+    collectionReadingOptions
+);
+
+seeder.import(collections)
+    .then(res => {
+        console.log(res, 'this is res');
+    })
+    .catch((err) => {
+        console.log(err, 'this is err');
+    })
+
 server.listen(3000);
 
 module.exports = server;
