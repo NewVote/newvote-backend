a74f9fb3ec402d15ab5191942282d33ce58378d4
Jamie <jamieplayer@gmail.com>
2019-07-14 09:34:15 +0100

    missing slash for email link

--- modules/future-leaders/future-leaders.server.controller.js
+++ modules/future-leaders/future-leaders.server.controller.js
@@ -93,7 +93,7 @@ exports.update = function (req, res) {
 var buildMessage = function (user, code, req) {
 	var messageString = '';
 	var url = req.protocol + '://' + req.get('host') + '/auth/signup/' + code;
-	console.log(url, 'this is url');
+
 	messageString += `<h3> You have been invited to join NewVote </h3>`;
 	messageString += `<p>To complete your account setup, just click the URL below or copy and paste it into your browser's address bar</p>`;
 	messageString += `<p><a href='${url}'>${url}</a></p>`;
