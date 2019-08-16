fca3224af9d17f2ac7eb5670a8e3694b642895a4
Jamie <jamieplayer@gmail.com>
2019-08-16 10:06:13 +0100

    rewrote saveRapidProfile - needed to get an organization object, cannot push req.organization._id onto organization array

--- modules/users/controllers/users.authentication.server.controller.js
+++ modules/users/controllers/users.authentication.server.controller.js
@@ -338,16 +338,14 @@ exports.oauthCallback = function (strategy) {
  * Helper function to create or update a user after AAF Rapid SSO auth
  */
 exports.saveRapidProfile = function (req, profile, done) {
-	const organization = req.organization;
-	if(!organization){
-		console.error('no organization in request body')
-	}
 
-	console.log('looking up user: ', profile.mail);
-	User.findOne({ email: profile.mail }, '-salt -password -verificationCode', function (err, user) {
-		if (err) {
-			return done(err);
-		} else {
+	const organizationPromise = Organization.findOne({_id : req.organization._id});
+	const userPromise = User.findOne({ email: profile.mail },  '-salt -password -verificationCode');
+
+	Promise.all([organizationPromise, userPromise])
+		.then((promises) => {
+			const [organization, user] = promises;
+
 			if (!user) {
 				console.log('no user, creating new account')
 				var possibleUsername = profile.cn || profile.displayname || profile.givenname + profile.surname || ((profile.mail) ? profile.mail.split('@')[0] : '');
@@ -368,9 +366,7 @@ exports.saveRapidProfile = function (req, profile, done) {
 					});
 
 					// And save the user
-					user.save(function (err) {
-						return done(err, user);
-					});
+					return user.save();
 				});
 			} else {
 				if(organization) {
@@ -388,14 +384,72 @@ exports.saveRapidProfile = function (req, profile, done) {
 					return done(new Error('ITA Match please login again'))
 				}
 				user.jti = profile.jti
-				user.save()
-					.then(user => {
-						return done(err, user);
-					})
+				return user.save();
 			}
-		}
-	});
+		})
+		.then((user) => {
+			return done(null, user);
+		})
+		.catch((err) => done(err));
 }
+	// const organization = req.organization;
+	// if(!organization){
+	// 	console.error('no organization in request body')
+	// }
+
+	// console.log('looking up user: ', profile.mail);
+	// User.findOne({ email: profile.mail }, '-salt -password -verificationCode', function (err, user) {
+	// 	if (err) {
+	// 		return done(err);
+	// 	} else {
+	// 		if (!user) {
+	// 			console.log('no user, creating new account')
+	// 			var possibleUsername = profile.cn || profile.displayname || profile.givenname + profile.surname || ((profile.mail) ? profile.mail.split('@')[0] : '');
+
+	// 			User.findUniqueUsername(possibleUsername, null, function (availableUsername) {
+	// 				console.log('generated username: ', availableUsername)
+	// 				user = new User({
+	// 					firstName: profile.givenname,
+	// 					lastName: profile.surname,
+	// 					username: profile.mail,
+	// 					displayName: profile.displayname,
+	// 					email: profile.mail,
+	// 					provider: 'aaf',
+	// 					ita: profile.ita,
+	// 					roles: ['user'],
+	// 					verified: true,
+	// 					organizations: [organization._id]
+	// 				});
+
+	// 				// And save the user
+	// 				user.save(function (err) {
+	// 					return done(err, user);
+	// 				});
+	// 			});
+	// 		} else {
+	// 			if(organization) {
+	// 				const orgExists = user.organizations.find((e) => {
+	// 					if(e) {
+	// 						return e._id.equals(organization._id)
+	// 					}
+	// 				});
+	// 				if (!orgExists) user.organizations.push(organization._id);
+	// 			}
+
+	// 			console.log('found existing user')
+	// 			// user exists update ITA and return user
+	// 			if (user.jti && user.jti === profile.jti) {
+	// 				return done(new Error('ITA Match please login again'))
+	// 			}
+	// 			user.jti = profile.jti
+	// 			user.save()
+	// 				.then(user => {
+	// 					return done(err, user);
+	// 				})
+	// 		}
+	// 	}
+	// });
+
 
 /**
  * Helper function to save or update a OAuth user profile
