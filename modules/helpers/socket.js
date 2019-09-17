0cb06592a73bea5e2a4d5208a1b4f011368922c4
Jamie Player <jamieplayer@gmail.com>
2019-09-17 13:00:37 +0100

    console.logs

--- modules/helpers/socket.js
+++ modules/helpers/socket.js
@@ -3,10 +3,9 @@ const socket = {
 };
 
 function socketSendData(request, data, event, channel) {
+    console.log('SENDING');
     const io = request.app.get('io');
     io.sockets.to(channel).emit(event, data);
 }
 
 module.exports = socket;
-
-
