ac3a3c7c7a0deccd096cccf9abc7919a3c7b62c1
Jamie Player <jamieplayer@gmail.com>
2019-09-23 16:59:47 +0100

    uninstalled sticky session

--- config/lib/app.js
+++ config/lib/app.js
@@ -7,8 +7,7 @@ let config = require('../config'),
     mongoose = require('./mongoose'),
     express = require('./express'),
     chalk = require('chalk'),
-    seed = require('./seed'),
-    sticky = require('sticky-session');
+    seed = require('./seed');
 
 function seedDB() {
     if (config.seedDB && config.seedDB.seed) {
@@ -36,15 +35,15 @@ module.exports.start = function start() {
     module.exports.init(function (app, db, config) {
         // Start the app by listening on <port>
 
-        // app.listen(config.port, function () {
-        //     // Logging initialization
-        //     console.log('--');
-        //     console.log(chalk.green(config.app.title));
-        //     console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
-        //     console.log(chalk.green('Port:\t\t\t\t' + config.port));
-        //     console.log(chalk.green('Database:\t\t\t' + config.db.uri));
-        //     console.log(chalk.green('App version:\t\t\t' + config.meanjs.version));
-        //     console.log('--');
-        // });
+        app.listen(config.port, function () {
+            // Logging initialization
+            console.log('--');
+            console.log(chalk.green(config.app.title));
+            console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
+            console.log(chalk.green('Port:\t\t\t\t' + config.port));
+            console.log(chalk.green('Database:\t\t\t' + config.db.uri));
+            console.log(chalk.green('App version:\t\t\t' + config.meanjs.version));
+            console.log('--');
+        });
     });
 };
