ad2593a9936e18c2bc6a058c643a8ea21357a925
Jamie Player <jamieplayer@gmail.com>
2019-11-05 17:37:32 +0000

    routes for progress components

---
+++ modules/progress-feed/progress-feed.server.routes.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Progress Feed Routes
+let path = require('path'),
+    config = require(path.resolve('./config/config')),
+    progressFeedController = require('./progress-feed.server.controller'),
+    jwt = require('express-jwt');
+
+module.exports = function(app) {
+
+    app.route('/api/progress-feed')
+        .all(
+            jwt({ secret: config.jwtSecret, credentialsRequired: false }),
+            policy.isAllowed
+        )
+        .get(progressFeedController.list)
+        .post(progressFeedController.create);
+
+    app.route('/api/progress/:progressFeedId')
+        .all(
+            jwt({ secret: config.jwtSecret, credentialsRequired: false }),
+            policy.isAllowed
+        )
+        .get(progressFeedController.read)
+        .put(progressFeedController.update)
+        .delete(progressFeedController.delete);
+
+    // Finish by binding the user middleware
+    app.param('progressFeedId', progressFeedController.progressFeedByID);
+};
+    
---
+++ modules/progress/progress.server.routes.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Progress Routes
+let path = require('path'),
+    config = require(path.resolve('./config/config')),
+    progressController = require('./progress.server.controller'),
+    jwt = require('express-jwt');
+
+module.exports = function(app) {
+
+    app.route('/api/progress')
+        .all(
+            jwt({ secret: config.jwtSecret, credentialsRequired: false }),
+            policy.isAllowed
+        )
+        .get(progressController.list)
+        .post(progressController.create);
+
+    app.route('/api/progress/:progressId')
+        .all(
+            jwt({ secret: config.jwtSecret, credentialsRequired: false }),
+            policy.isAllowed
+        )
+        .get(progressController.read)
+        .put(progressController.update)
+        .delete(progressController.delete);
+
+    // Finish by binding the user middleware
+    app.param('progressId', progress.progressByID);
+};
