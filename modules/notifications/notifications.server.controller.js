7c759ffa46a8939ef3a5ab7bf0fe2393ad1e981f
Jamie <jamieplayer@gmail.com>
2020-05-06 17:11:37 +0100

    regex

--- modules/notifications/notifications.server.controller.js
+++ modules/notifications/notifications.server.controller.js
@@ -177,10 +177,6 @@ const sendPushNotification = (notification, organization) => {
 }
 
 function stripHtml(html){
-    // Create a new div element
-    var temporalDivElement = document.createElement("div");
-    // Set the HTML content with the providen
-    temporalDivElement.innerHTML = html;
-    // Retrieve the text property of the element (cross-browser support)
-    return temporalDivElement.textContent || temporalDivElement.innerText || "";
+    // https://stackoverflow.com/a/5002161
+    return html.replace(/(<([^>]+)>)/ig,'');
 }
